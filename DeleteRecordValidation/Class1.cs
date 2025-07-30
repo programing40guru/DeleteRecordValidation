0d7c157968a1ca112131b854c5bf0c1b71ca7578
Dabhi Mehul <mehuldabhi00000@gmail.com>
2025-07-30 13:55:10 +0530

    final Code Tested
    Enhance DeleteRecordValidation plugin functionality
    
    - Added using directives for identity model and remoting.
    - Introduced `RecordId` variable for entity ID processing.
    - Updated owner retrieval to include additional attributes.
    - Enhanced audit data handling and user information logging.
    - Refined attribute processing logic, including `ownerid`.
    - Implemented `CreateEmailActivity` method for email notifications.
    - Formatted email body with relevant student details and subject.
    - Overall improvements to data retrieval, processing, and notifications.

--- DeleteRecordValidation/Class1.cs
+++ DeleteRecordValidation/Class1.cs
@@ -2,11 +2,17 @@
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Metadata;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Services;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
-using System.Text.Json;
+using System.Xml.Linq;
+
+ 
 
 namespace DeleteRecordValidation
 {
@@ -36,12 +42,12 @@ namespace DeleteRecordValidation
                     try
                     {
                         tracingService.Trace("I am Plugin");
-                       
+
                         Guid RecordId = entity.Id;
                         /*
                         string EntityName = entity.LogicalName;
 
-                        Guid userId = context.InitiatingUserId;
+                        
 
                         var FullEntity = service.Retrieve(EntityName, RecordId, new ColumnSet("ownerid"));
 
@@ -63,26 +69,44 @@ namespace DeleteRecordValidation
 
                         }
 
-                        */
+                        */ 
+
                         EntityCollection Student = GetAuditDelatedData(service, RecordId);
 
                         string Name = null;
-                        string Subject = null; 
-                        string Marks = null;
-                        decimal Scholarship= 0 ;
-                        decimal Parcentage =0;
+                        string Subject = null;
+                        decimal Marks = 0;
+                        decimal Scholarship = 0;
+                        decimal Parcentage = 0;
+
                         string Teacher = null;
-                        string Birthday = null; 
+                        Guid TeacherId = Guid.Empty;
+                        string TeacherEntityName = null;
+                        string TeacherName = null;
+
+                        string Birthday = null;
+                        string Owner = null;
+                        Guid OwnerId = Guid.Empty;
+                        string OwnerEntityName = null;
+                        string OwnerName = null;
+
+                        string UserName = null;
+                        Guid UserId = Guid.Empty;
+                        string UserEntityName = null;
+
 
                         foreach (var student in Student.Entities)
                         {
                             var auditId = student.Id;
                             var createdOn = student.GetAttributeValue<DateTime>("createdon");
                             var operation = student.GetAttributeValue<OptionSetValue>("operation")?.Value;
-                            var user = student.GetAttributeValue<EntityReference>("userid")?.Name;
+                            UserName = student.GetAttributeValue<EntityReference>("userid")?.Name;
+                            UserId = student.GetAttributeValue<EntityReference>("userid")?.Id ?? Guid.Empty;
+                            UserEntityName = student.GetAttributeValue<EntityReference>("userid")?.LogicalName;
+
                             var Data = student.GetAttributeValue<string>("changedata");
 
-                            tracingService.Trace($"AuditID: {auditId}, Date: {createdOn}, Operation: {operation}, User: {user}");
+                            tracingService.Trace($"AuditID: {auditId}, Date: {createdOn}, Operation: {operation}, User: {UserName} :{UserId}:{UserEntityName}");
                             tracingService.Trace(Data);
 
                             JsonDocument doc = JsonDocument.Parse(Data);
@@ -92,27 +116,30 @@ namespace DeleteRecordValidation
                             foreach (var x in changedAttributes.EnumerateArray())
                             {
                                 string logicalNames = x.GetProperty("logicalName").GetString();
-                               // tracingService.Trace("" + logicalNames);
+                                // tracingService.Trace("" + logicalNames);
 
                                 switch (logicalNames)
                                 {
                                     case "new_name":
                                         {
-                                            Name = x.GetProperty("oldValue").GetString()?? null;
+                                            Name = x.GetProperty("oldValue").GetString() ?? null;
                                             break;
                                         }
                                     case "crf39_subjectname":
                                         {
-                                            Subject = x.GetProperty("oldValue").GetString()?? null;
+                                            Subject = x.GetProperty("oldValue").GetString() ?? null;
                                             break;
                                         }
                                     case "new_teacher":
                                         {
-                                            Teacher = x.GetProperty("oldValue").GetString()?? null;
+                                            Teacher = x.GetProperty("oldValue").GetString() ?? null;
                                             if (Teacher != null)
                                             {
-                                                Entity teacher = service.Retrieve(Teacher.Split(',')[0], new Guid(Teacher.Split(',')[1]), new ColumnSet("new_name"));
-                                                Teacher = teacher.GetAttributeValue<string>("new_name");
+                                                TeacherEntityName = Teacher.Split(',')[0];
+                                                TeacherId = new Guid(Teacher.Split(',')[1]);
+                                                
+                                                Entity teacher = service.Retrieve(TeacherEntityName,TeacherId, new ColumnSet("new_name"));
+                                                TeacherName = teacher.GetAttributeValue<string>("new_name");
                                             }
                                             break;
                                         }
@@ -122,16 +149,16 @@ namespace DeleteRecordValidation
                                             if (Birthday != null)
                                             {
                                                 Birthday = Birthday.Split(' ')[0];
-                                            } 
+                                            }
                                             break;
                                         }
                                     case "new_maxscholarshipamount":
                                         {
                                             string Scholarships = x.GetProperty("oldValue").GetString();
-                                            if (Scholarships !=null)
+                                            if (Scholarships != null)
                                             {
                                                 Scholarship = Convert.ToDecimal(Scholarships);
-                                                Scholarship = Math.Round(Scholarship, 2); 
+                                                Scholarship = Math.Round(Scholarship, 2);
                                             }
                                             break;
                                         }
@@ -141,26 +168,43 @@ namespace DeleteRecordValidation
                                             if (Parcentages != null)
                                             {
                                                 Parcentage = Convert.ToDecimal(Parcentages);
-                                               Parcentage = Math.Round(Parcentage, 2);   
+                                                Parcentage = Math.Round(Parcentage, 2);
                                             }
 
                                             break;
                                         }
                                     case "new_marks":
                                         {
-                                            Marks = x.GetProperty("oldValue").GetString() ?? null;
+                                            string Mark = x.GetProperty("oldValue").GetString() ?? null;
+                                            if (Mark != null)
+                                            {
+                                                Marks = Convert.ToDecimal(Mark);
+                                                Marks = Math.Round(Marks, 2);
+                                            }
+                                            break;
+                                        }
+                                    case "ownerid":
+                                        {
+                                            Owner = x.GetProperty("oldValue").GetString() ?? null;
+                                            if (Owner != null)
+                                            {
+                                                OwnerId = new Guid(Owner.Split(',')[1]);
+                                                OwnerEntityName = Owner.Split(',')[0]; 
+                                                Entity Owners = service.Retrieve(OwnerEntityName,OwnerId, new ColumnSet("fullname"));
+                                                OwnerName = Owners.GetAttributeValue<string>("fullname");
+                                            }
                                             break;
                                         }
                                     default: {
-                                            
-                                            break; 
-                                    }
+
+                                            break;
+                                        }
 
                                 }
-                                 
+
                             }
-                            tracingService.Trace($"{Name}{Subject}{Marks}{Scholarship}{Parcentage}{Teacher}{Birthday}");
- 
+                            tracingService.Trace($"{Name},{Subject},{Marks},{Scholarship},{Parcentage},{Teacher},{Birthday}");
+                            CreateEmailActivity(service, OwnerId, OwnerEntityName, OwnerName, Name, Subject, Marks, Scholarship, Parcentage, TeacherName,TeacherId, TeacherEntityName,UserName, UserId, UserEntityName, Birthday);
                         }
 
                         tracingService.Trace("try CaseFinished");
@@ -194,8 +238,47 @@ namespace DeleteRecordValidation
                 <order attribute=""createdon"" descending=""true"" />
                 </entity>
             </fetch>";
-            
+
             return service.RetrieveMultiple(new FetchExpression(fetchXml));
+        }
+
+        public static void  CreateEmailActivity(IOrganizationService service, Guid OwnerId,string  OwnerEntityName, string OwnerName, string Name, string Subject,decimal Marks,decimal  Scholarship,decimal Parcentage, string TeacherName, Guid TeacherId , string TeacherEntityName,string  UserName, Guid UserId, string UserEntityName, string Birthday) {
+
+            Entity Emails = new Entity("email");
+
+            var FromPointer = new Entity("activityparty");
+            FromPointer["partyid"] = new EntityReference(OwnerEntityName, OwnerId);
+
+            var ToOwner = new Entity("activityparty");
+            ToOwner["partyid"] = new EntityReference(UserEntityName, UserId);
+
+ 
+            Emails["to"] = new EntityCollection(new List<Entity> { ToOwner });
+            if (TeacherName != null)
+            {
+                
+                Emails["regardingobjectid"] = new EntityReference(TeacherEntityName, TeacherId);
+            } 
+            Emails["from"] = new EntityCollection(new List<Entity> { FromPointer });
+
+ 
+            Emails["ownerid"] = new EntityReference(UserEntityName, UserId);
+
+            //Email Body Operations 
+            Name = Name ?? "Name is not available";
+            Birthday = "Birthday : " + Birthday;
+            TeacherName = TeacherName ?? "Teacher Name is not available";
+            Subject = Subject ?? "Subject is not available";
+
+            string Body = $"Hello {TeacherName},\nI am writing this email to inform you about the student termination and record deletion.\nBelow are the student details:\n\nStudent Name: {Name}\n{Birthday}\nMarks: {Marks}\nPercentage: {Parcentage}\nSubject: {Subject}\nScholarship: {Scholarship}\nTeacher Name: {TeacherName}\n\n Thank you.\nRegards,\n{OwnerName}";
+          
+            Emails["subject"] = $"{Name}'s Record is Deleted";
+            Emails["description"] = Body;
+
+            Emails["directioncode"] = true;
+
+            service.Create(Emails);
+
         }
     }
 }
